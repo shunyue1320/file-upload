32746495c3907ac1f3094484bab8346a0044e340
shunyue1320 <1277952981@qq.com>
2021-02-20 00:13:31 +0800

    frontend add login layout

---
+++ frontend/layouts/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>导航</div>
+    <Nuxt />
+    <div>页尾</div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ frontend/pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-form class="login-form" :rules="form">
+      <el-form-item props="email">
+        <span>
+          <li class="el-icon-mobile"></li>
+        </span>
+        <el-input placeholder="邮箱"></el-input>
+      </el-form-item>
+      <el-form-item props="password">
+        <span>
+          <li class="el-icon-lock"></li>
+        </span>
+        <el-input placeholder="密码"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'login',
+  data () {
+    return {
+      form: []
+    }
+  }
+}
+</script>
+
+<style>
+.login-form {
+  width: 800px;
+  margin: 50px auto;
+}
+</style>
