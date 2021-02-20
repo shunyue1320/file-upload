5c9cd75db899f9e3a3831553988182daf59f911f
shunyue1320 <1277952981@qq.com>
2021-02-20 21:39:14 +0800

    backend add captche api

---
+++ backend/app/controller/util.js
@@ -0,0 +1,20 @@
+const Controller = require('egg').Controller
+const svgCaptcha = require('svg-captcha')
+
+class UtilController extends Controller {
+  async captcha() {
+    const { ctx } = this
+    const captcha = svgCaptcha.create({
+      size: 4,
+      width: 100,
+      height: 40,
+      fontSize: 50,
+      noise: 3
+    })
+    ctx.session.captcha = captcha.text
+    ctx.response.type = 'image/svg+xml'
+    ctx.body = captcha.data
+  }
+}
+
+module.exports = UtilController
